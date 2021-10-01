aeb7c4562a1acd32d6fea1ec724d92a8007220dd
Hanshen Wang <19878148+HanshenWang@users.noreply.github.com>
2021-10-01 15:26:55 -0400

    Docs: add license file headers to comply with LGPL best practices

--- application.lisp
+++ application.lisp
@@ -1,6 +1,25 @@
-;;;; ================
 ;;;; application.lisp
-;;;; ================
+;;;
+;;; Copyright (c) 2021 Hanshen Wang.
+;;;
+;;; Author: Hanshen Wang <Hanshen@HanshenWang.com>
+;;; URL: https://github.com/HanshenWang/project-isidore
+;;;
+;;; This file is part of Project Isidore.
+;;;
+;;; Project Isidore is free software: you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation, either version 3 of the License, or
+;;; (at your option) any later version.
+;;;
+;;; Project Isidore is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with Project Isidore.  If not, see <https://www.gnu.org/licenses/>.
+
 ;;;; initialize-application is the function launched by the heroku buildpack. This has been moved to heroku.lisp as the function must be under the "cl-user" namespace and not project-isidore.
 (defpackage #:project-isidore
   ;; :use means there is no need to prefix hunchentoot:define-easy-handler and instead use just define-easy-handler
