0b535c3505f2cbfa7b184734f85b4dc2ea66ded0
Hanshen Wang <19878148+HanshenWang@users.noreply.github.com>
2021-10-06 02:35:40 -0400

    Build: Remove support for Clozure Common Lisp
    In preparation to simplify and understand my build process, I am removing
    support for CCL (& Roswell in the upcoming commits). If I want to add
    portability in the future it will be through less #+sbcl or #+ccl conditionals
    but rather through the ever expanding mission creep of ASDF.
    
    CI is another story. Core app logic should be implementation agnostic and here
    Roswell does a great service to testing. As per my current use case, I don't
    foresee deploying on more than SBCL likely in the near future. Therefore as
    barebones as my application already is, I desire to perfect it more by carving
    away the fat.
    
    Taking out the complexity of Roswell so that the build is as simple as needs be
    should save me some time in the future. Fingers crossed.

--- bin/compile.lisp
+++ bin/compile.lisp
@@ -21,40 +21,33 @@
 ;;; along with Project Isidore.  If not, see <https://www.gnu.org/licenses/>.
 
 (in-package :cl-user)
+(require :sb-posix)
 (require 'asdf)
 
-(flet ((env-to-dirs (x)
-         (pathname-directory (pathname (concatenate 'string
-						    #+sbcl (sb-posix:getenv x)
-						    #+ccl (getenv x) "/")))))
-  (defvar *build-dir* (env-to-dirs "BUILD_DIR"))
-  (defvar *cache-dir* (env-to-dirs "CACHE_DIR"))
-  (defvar *buildpack-dir* (env-to-dirs "BUILDPACK_DIR")))
 
 (format t "~&  *build-dir* = ~a" (make-pathname :directory *build-dir*))
 (format t "~&  *cache-dir* = ~a" (make-pathname :directory *cache-dir*))
 (format t "~&  *buildpack-dir* = ~a" (make-pathname :directory *buildpack-dir*))
+  (flet ((env-to-dirs (x)
+           (pathname-directory (pathname (concatenate 'string (sb-posix:getenv x) "/")))))
+    (defvar *buildpack-dir* (env-to-dirs "BUILDPACK_DIR"))
+    (defvar *build-dir* (env-to-dirs "BUILD_DIR"))
+    (defvar *cache-dir* (env-to-dirs "CACHE_DIR")))
 
-;;; Tell ASDF to store binaries in the cache dir.
-#+ccl (ccl:setenv "XDG_CACHE_HOME" (concatenate 'string (getenv "CACHE_DIR") "/.asdf/"))
-#+sbcl (sb-posix:setenv "XDG_CACHE_HOME" (concatenate 'string (sb-posix:getenv "CACHE_DIR")
-						     "/.asdf/") 1)
 
 ;;; Notify ASDF that our build and cache dir is an awesome place to find asf files.
 (asdf:initialize-source-registry `(:source-registry
                                     (:tree ,(make-pathname :directory *build-dir*))
                                     (:tree ,(make-pathname :directory *cache-dir*))
                                     :inherit-configuration))
+  ;; Tell ASDF to store binaries in the cache dir.
+  (sb-posix:setenv "XDG_CACHE_HOME" (concatenate 'string (sb-posix:getenv "CACHE_DIR")
+                                                 "/.asdf/") 1)
 
 ;;; App can redefine this to do runtime initializations
 (defun initialize-application (&key port)
   (declare (ignore port)))
 
-;;; Default toplevel, app can redefine.
-(defun heroku-toplevel ()
-  (initialize-application :port (parse-integer  #+sbcl (sb-posix:getenv "PORT")
-						#+ccl (getenv "PORT")))
-  (loop (sleep 600)))			;sleep forever
 
 (defvar *root* "/app")			;this is always the app root on Heroku now?
 
@@ -81,13 +74,13 @@
   (setf *acceptor*
         (hunchentoot:start (make-instance 'hunchentoot:easy-acceptor :port port))))
 
+;;; Default toplevel, app can redefine.
+(defun application-toplevel ()
+  (initialize-application :port (parse-integer (sb-posix:getenv "PORT")))
+  (loop (sleep 600))) ; sleep forever
+
 ;;; Save the application as an image
 (let ((app-file (make-pathname :directory *build-dir* :defaults "lispapp")))
-  ;; note that the buildpack's bin/release refers to this application name.
-  (format t "~&* create slug's ./lispapp via save-application")
-  #+ccl (save-application app-file
-			  :prepend-kernel t
-			  :toplevel-function #'heroku-toplevel)
-  #+sbcl (save-lisp-and-die app-file
-			    :toplevel #'heroku-toplevel
-			    :executable t))
+  (save-lisp-and-die app-file :toplevel #'application-toplevel :executable t))
+
+(format t "~&        ====== END OF COMPILE.LISP ======")
